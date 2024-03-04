fbe73e4219b8aed22b006a93ec7e5ba70db552e1
Domiuau <Domiuaunoob@outlook.com>
2024-03-04 00:18:13 -0300

    carro

--- src/main/java/br/com/aplicacao/demo/dto/usuario/DadosUsuarioDTO.java
+++ src/main/java/br/com/aplicacao/demo/dto/usuario/DadosUsuarioDTO.java
@@ -8,6 +8,13 @@ public record DadosUsuarioDTO(String apelido, String nomeDeUsuario, String email
         this (usuario.getApelido(), usuario.getUsername(), usuario.getEmail(), usuario.getTelefone());
     }
 
+    public DadosUsuarioDTO(String apelido, String nomeDeUsuario, String email, String telefone) {
+        this.apelido = apelido;
+        this.nomeDeUsuario = nomeDeUsuario;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
 
 
 }
